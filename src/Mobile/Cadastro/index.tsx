4ee7a8718ac52690f1d7643bc6164eef5600f39d
Denis Oliveira <denisdev3loper@gmail.com>
2024-04-19 20:18:40 -0300

    teste form cadastro api

--- src/Mobile/Cadastro/index.tsx
+++ src/Mobile/Cadastro/index.tsx
@@ -5,22 +5,39 @@ import { Div } from './styles'
 interface Member {
   id: number
   name: string
+  email: string
+  telefone: string
 }
 
 const Cadastro = () => {
   const [members, setMembers] = useState<Member[]>([])
   const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [telefone, setTelefone] = useState<string>('')
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
+  }
+
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
+  }
+
+  const handleChangeTelefone = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTelefone(event.target.value)
   }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     try {
-      await axios.post('http://localhost:5000/add_member', { name })
+      await axios.post(
+        'https://66204b2c3bf790e070af7e54.mockapi.io/api/cadastro',
+        { name, email, telefone }
+      )
       alert('Member added successfully!')
       setName('')
+      setEmail('')
+      setTelefone('')
       fetchMembers() // Buscar membros novamente após adicionar um novo membro
     } catch (error) {
       console.error('Failed to add member:', error)
@@ -31,8 +48,8 @@ const Cadastro = () => {
   const fetchMembers = async () => {
     try {
       const response = await axios.get<Member[]>(
-        'http://localhost:5000/members'
-      ) // Especificar o tipo de resposta como Member[]
+        'https://66204b2c3bf790e070af7e54.mockapi.io/api/cadastro'
+      )
       setMembers(response.data)
     } catch (error) {
       console.error('Failed to fetch members:', error)
@@ -41,7 +58,9 @@ const Cadastro = () => {
 
   const deleteMember = async (id: number) => {
     try {
-      await axios.delete(`http://localhost:5000/members/${id}`) // Enviar a requisição DELETE para a rota /members/:id
+      await axios.delete(
+        `https://66204b2c3bf790e070af7e54.mockapi.io/api/cadastro/${id}`
+      )
       fetchMembers() // Atualizar a lista de membros após a exclusão
     } catch (error) {
       console.error('Failed to delete member:', error)
@@ -58,7 +77,15 @@ const Cadastro = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" value={name} onChange={handleChange} />
+          <input type="text" value={name} onChange={handleChangeName} />
+        </label>
+        <label>
+          Email:
+          <input type="text" value={email} onChange={handleChangeEmail} />
+        </label>
+        <label>
+          Telefone:
+          <input type="text" value={telefone} onChange={handleChangeTelefone} />
         </label>
         <button type="submit">Add Member</button>
       </form>
@@ -66,7 +93,7 @@ const Cadastro = () => {
       <ul>
         {members.map((member) => (
           <li key={member.id}>
-            {member.name} {member.id}
+            {member.name} - {member.email} - {member.telefone}
             <button onClick={() => deleteMember(member.id)}>Delete</button>
           </li>
         ))}
