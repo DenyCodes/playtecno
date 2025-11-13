ab7abcb710ea8d9f6b860aaf8cdbe4bf00b48d82
Denis Oliveira <denisdev3loper@gmail.com>
2025-11-13 07:28:44 -0300

    update

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,6 +3,7 @@ import 'styled-components'
 declare module 'styled-components' {
   export interface DefaultTheme {
     cores: {
+      primaria: Interpolation<FastOmit<DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>, never>>
       up: string
       card: string
       fundo: string
