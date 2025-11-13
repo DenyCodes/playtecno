b33cfcf073830476c9858efbe924cc72dda8a0b2
Denis Oliveira <denisdev3loper@gmail.com>
2025-11-13 06:42:26 -0300

    scsa

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,15 +1,21 @@
-export interface DefaultTheme {
-  cores: {
-    up: string
-    card: string
-    fundo: string
-    pb: string
-    preto: string
-    texto: string
-    textoSecundario: string
-    titulo: string
-    h2: string
-    span: string
-    iconFilter: string
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    cores: {
+      up: string
+      card: string
+      fundo: string
+      pb: string
+      preto: string
+      texto: string
+      textoSecundario: string
+      titulo: string
+      h2: string
+      span: string
+      iconFilter: string
+    }
   }
+
 }
+
