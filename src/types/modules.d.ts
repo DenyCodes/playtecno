1d1170940fe808d0339c912bdf64f34cad2b3f55
Denis Oliveira <denisdev3loper@gmail.com>
2025-11-13 07:18:14 -0300

    update

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -7,3 +7,7 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
